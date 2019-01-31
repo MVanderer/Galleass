001afa1be1c00ed2621eb0a6ecb5a0077e642a23
Vadim Flaks <Max@Vadims-MacBook-Pro.local>
2019-01-31 15:51:05 -0600

    second attempt

--- wwwroot/js/WorldDraw/game.js
+++ wwwroot/js/WorldDraw/game.js
@@ -1,3 +1,33 @@
+// CANVAS Initialization
+// context to be passed into functions
+let canvas = document.querySelector("canvas")
+let c = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// canvas.addEventListener("click", (e) => {
+//     console.log("x: " + e.x + ", y: " + e.y);
+// });
+
+let showingCoords = false;
+
+let size = 50;//will need to calculate based on size and range
+
+let currentX = 0;// will change, 
+let currentY = 0;// dependent on the player
+
+let xRenderRange = 4;//same as size
+let yRenderRange = 4;//change later
+
+let currentOffsetX = 0;//this is for animating
+let currentOffsetY = 0;//movement.
+let directionImg = "southwest.png";
+let shifting = false;
+
+//this is the actual meat of the rendering pipeline
+let renderPlan = new renderView(currentX, currentY, xRenderRange, yRenderRange);
+console.log(renderPlan);
+
 //User controls section
 document.addEventListener("keydown", (e) => {
     console.log(e);
@@ -13,7 +43,7 @@ function movePlayerInDb(originX, originY, rangeX, rangeY) {
     let url = "/RenderMap/" + originX + "/" + originY + "/" + rangeX + "/" + rangeY;
     xhr.onload = () => {
         let world = JSON.parse(xhr.response);
-        console.log(world);
+        return world;
 
     }
 
@@ -21,62 +51,97 @@ function movePlayerInDb(originX, originY, rangeX, rangeY) {
     xhr.send(null);
 }
 
+function outOfBounds(x, y) {
+    console.log(x+" "+y);
+    console.log(renderPlan.playerX);
+    console.log(renderPlan.playerY);
+    
+    console.log(renderPlan.layout[renderPlan.playerY][renderPlan.playerX]);
+    console.log(renderPlan.layout[renderPlan.playerY][renderPlan.playerX]);
+    
+    
+    if (renderPlan.layout[renderPlan.playerY-1 + y]) {
+
+        if (renderPlan.layout[renderPlan.playerY-1 + y][renderPlan.playerX-1 + x]) {
+            return false;
+        }
+    }
+    else {
+        return true;
+    }
+}
+
 function movePlayer(direction) {
+    directionImg = direction + ".png";
     switch (direction) {
         case "north":
-            currentY--;
-
+            if (!outOfBounds(0, - 1)) {
+                currentY--;
+            }
             break;
         case "northeast":
-            currentY--;
-
+            if (currentX % 2 == 0) {
+                if (!outOfBounds(1, - 1)) {
+                    currentX++;
+                    currentY--;
+                }
+            }
+            else {
+                if (!outOfBounds(1, 0)) {
+                    currentX++;
+                }
+            }
             break;
         case "northwest":
-            currentY--;
-
+            if (currentX % 2 == 0) {
+                if (!outOfBounds(- 1, - 1)) {
+                    currentX--;
+                    currentY--;
+                }
+            }
+            else {
+                if (!outOfBounds(- 1, 0)) {
+                    currentX--;
+                }
+            }
             break;
         case "south":
-            currentY--;
-
+            if (!outOfBounds(0, 1)) {
+                currentY++;
+            }
             break;
         case "southwest":
-            currentY--;
-
+            if (currentX % 2 == 0) {
+                if (!outOfBounds(- 1, - 1)) {
+                    currentX--;
+                }
+            }
+            else {
+                if (!outOfBounds(- 1, 1)) {
+                    currentY++;
+                    currentX--;
+                }
+            }
             break;
         case "southeast":
-            currentY--;
-            break;
+            if (currentX % 2 == 0) {
+                if (!outOfBounds(1, 0)) {
+                    currentX++;
+                }
+            }
+            else {
+                if (!outOfBounds(1, 1)) {
+                    currentX++;
+                    currentY++;
+                }
+            } break;
         default:
             break;
     }
-    movePlayerInDb(currentX, currentY, xRenderRange, yRenderRange);
+    renderPlan.getGrid(currentX, currentY, xRenderRange, yRenderRange);
 }
 
-
-// CANVAS STUFF STARTS HERE
-//All pretty standard stuff, 2D canvas the size of the window, pre-animation frame. NOTE!!! Never not be box-sizing them border-boxes. Otherwise the canvas won't fit.
-let canvas = document.querySelector("canvas")
-let c = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-canvas.addEventListener("click", (e) => {
-    console.log("x: " + e.x + ", y: " + e.y);
-});
-
-let showingCoords = true;
-let size = 50;
-
-let currentX = 0;
-let currentY = 0;
-let xRenderRange = 4;
-let yRenderRange = 4;
-let currentOffsetX = 0;
-let currentOffsetY = 0;
-let direction = "none";
-let shifting = false;
-
-console.log(myMap);
+// console.log(myMap);
 
 function animate() {
     //basic animation setup
@@ -101,8 +166,28 @@ function animate() {
     if (shifting) {
 
     }
+    if (renderPlan.layout) {
+        renderPlan.render(
+            c,
+            canvas.width,
+            canvas.height,
+            size,
+            currentOffsetX,
+            currentOffsetY);
 
-    myMap.drawGrid(c, currentX, currentY, xRenderRange, yRenderRange, currentOffsetX, currentOffsetY);
+    }
+    let shipAvatar = new Image();
+    shipAvatar.src = "/img/gal-avatar/" + directionImg;
+    c.drawImage(
+        shipAvatar,
+        canvas.width / 2 - size,
+        canvas.height / 2 - (Math.sqrt(3) * size) / 2,
+        2 * size,
+        Math.sqrt(3) * size
+    )
+
+    // myMap.drawGrid(c, currentX, currentY, xRenderRange, yRenderRange, currentOffsetX, currentOffsetY);
+
     if (showingCoords) {
         c.rect(canvas.width / 2, canvas.height / 2, 3, 3);
         c.strokeStyle = "red";
--- wwwroot/js/WorldDraw/world.js
+++ wwwroot/js/WorldDraw/world.js
@@ -9,6 +9,108 @@ class mapPoint {
     }
 }
 
+class renderView {
+    constructor(originX, originY, rangeX, rangeY) {
+        let xhr = new XMLHttpRequest();
+        let url = "/RenderMap/" + originX + "/" + originY + "/" + rangeX + "/" + rangeY;
+        xhr.onload = () => {
+            let world = JSON.parse(xhr.response);
+            this.layout = world;
+            this.width = this.layout[0].length;
+            this.height = this.layout.length;
+            this.playerX = Math.ceil(this.width / 2);
+            this.playerY = Math.ceil(this.height / 2);
+        }
+
+        xhr.open("GET", url);
+        xhr.send(null);
+
+    }
+
+    getGrid(originX, originY, rangeX, rangeY) {
+        console.log("getting grid");
+        
+        let xhr = new XMLHttpRequest();
+        let url = "/RenderMap/" + originX + "/" + originY + "/" + rangeX + "/" + rangeY;
+        xhr.onload = () => {
+            let world = JSON.parse(xhr.response);
+            this.layout = world;
+            this.width = this.layout[0].length;
+            this.height = this.layout.length;
+            this.playerX = Math.ceil(this.width / 2);
+            this.playerY = Math.ceil(this.height / 2);
+        }
+
+        xhr.open("GET", url);
+        xhr.send(null);
+    }
+
+    render(
+        ctx,
+        cWidth, cHeight,//canvas dimensions
+        sideLength,
+        xOffset = 0,//animation stuff
+        yOffset = 0
+    ) {
+        //Odd-Q. Very important.“odd-q” = vertical layout shoves odd columns down
+
+        //offsets are supposed to be between -1 and 1
+        xOffset = xOffset * ((3 / 2) * size);
+        if (this.playerX % 2 != 0) {
+            yOffset = yOffset * (Math.sqrt(3) * sideLength) * (-1 + 0.5);
+        }
+        else {
+            yOffset = yOffset * (Math.sqrt(3) * sideLength) * (-1 - 0.5);
+        }
+        //start at the center of the canvas
+        let x = (cWidth / 2) - xOffset;
+        let y = (cHeight / 2) + yOffset;
+
+        //the following code is a bad attempt at replicating the function that rendered a segment of the entire available map placing the current cell into the center and rendering +- range from that cell
+        //THIS NEEDS TO BE REWRITTEN
+        for (let ver = 0; ver < this.layout.length; ver++) {
+            for (let hor = 0; hor < this.layout[ver].length; hor++) {
+                let type = "sea";
+                let url = "sea-hex.png";
+                if (this.layout[ver][hor]) {
+                    type = this.layout[ver][hor].Type;
+                    url = this.layout[ver][hor].ImageURL;
+
+                }
+                let xRange=hor-this.playerX+1;
+                let yRange=ver-this.playerY+1;
+                if ((this.layout[this.playerY][this.playerX]==undefined)){
+                    console.log("this.layout[this.playerY][this.playerX]");
+                    console.log(this.playerX+" "+this.playerY);
+                    
+                    
+                    console.log(this);
+                    
+                }
+
+
+                let rowCheck = ((this.layout[this.playerY][this.playerX].xCoord) % 2) == 0;
+
+                let flip1 = (yRange + (0.5 * (Math.abs(xRange) % 2)));
+                let flip2 = (yRange - (0.5 * (Math.abs(xRange) % 2)));
+
+                if (rowCheck) {
+                    flip1 = flip2;
+                }
+                drawHex(
+                    ctx,//canvas context
+                    x + xRange * (3 / 2) * size,
+                    y + (Math.sqrt(3) * size) * flip1,
+                    sideLength,
+                    type,
+                    url,
+                    this.layout[this.playerY][this.playerX].xCoord + xRange-1,
+                    this.layout[this.playerY][this.playerX].yCoord + yRange-1);
+            }
+        }
+    }
+}
+
 class gameMap {
     constructor(w, h, locations) {
         this.layout = [];
@@ -37,17 +139,24 @@ class gameMap {
         }
     }
 
-    drawGrid(ctx, xPos, yPos, horRange, vertRange, xOffset = 0, yOffset = 0) {
+    drawGrid(ctx,
+        xPos,
+        yPos,
+        horRange,
+        vertRange,
+        xOffset = 0,
+        yOffset = 0) {
         //Odd-Q!!! Very important.“odd-q” vertical layout shoves odd columns down
         xOffset = xOffset * ((3 / 2) * size);
         if (xPos % 2 != 0) {
-            yOffset = yOffset * (Math.sqrt(3) * size)*(-1+0.5);
+            yOffset = yOffset * (Math.sqrt(3) * size) * (-1 + 0.5);
         }
         else {
-            yOffset = yOffset * (Math.sqrt(3) * size)*(-1-0.5);
+            yOffset = yOffset * (Math.sqrt(3) * size) * (-1 - 0.5);
         }
         let x = (canvas.width / 2) - xOffset;
         let y = (canvas.height / 2) + yOffset;
+
         for (let ver = 0 - vertRange; ver <= vertRange; ver++) {
             for (let hor = 0 - horRange; hor <= horRange; hor++) {
                 let type = "sea";
@@ -60,6 +169,7 @@ class gameMap {
                 }
                 let flip1 = (ver + (0.5 * (Math.abs(hor) % 2)));
                 let flip2 = (ver - (0.5 * (Math.abs(hor) % 2)));
+
                 if (xPos % 2 != 0) {
                     flip1 = flip2;
                 }
@@ -77,20 +187,10 @@ class gameMap {
     }
 }
 //there will have to be a better way to make these, but this is the meat - actual locations in the sea
-let newLoc = [
-    new mapPoint(1, 1, "land", "land-hex.png"),
-    new mapPoint(3, 1, "land", "land-hex.png"),
-    new mapPoint(4, 1, "land", "land-hex.png"),
-    new mapPoint(5, 1, "land", "land-hex.png"),
-];
-
-let myMap = new gameMap(9, 12, newLoc);
 
 //this is just a helper. once the math for checking inside a hex is done, will replace this. Food for thught: maybe check the clicks for being inside of a circle, rather than a hex? Math is simple
 
 function drawHex(ctx, xCoord, yCoord, sideSize, type, url = "sea-hex.png", arrX = "", arrY = "") {
-    console.log(ctx);
-    
     let img = new Image();
     img.src = "/img/" + url;
 
@@ -102,11 +202,11 @@ function drawHex(ctx, xCoord, yCoord, sideSize, type, url = "sea-hex.png", arrX 
         yCoord - (Math.sqrt(3) * sideSize) / 2,
         2 * sideSize,
         Math.sqrt(3) * sideSize
-        );
+    );
 
 
-        ctx.beginPath();
-        ctx.moveTo(xCoord + sideSize * Math.cos(0), yCoord + sideSize * Math.sin(0));
+    ctx.beginPath();
+    ctx.moveTo(xCoord + sideSize * Math.cos(0), yCoord + sideSize * Math.sin(0));
 
     for (let side = 0; side < 7; side++) {
         ctx.lineTo(xCoord + sideSize * Math.cos(side * 2 * Math.PI / 6), yCoord + sideSize * Math.sin(side * 2 * Math.PI / 6));
