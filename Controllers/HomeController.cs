f130646dcc522cd3b4e0e9901e4b684eaca6b9ff
Vadim Flaks <Max@Vadims-MacBook-Pro.local>
2019-02-01 07:46:14 -0600

    Restyled new game page, changed custom font on headers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -162,11 +162,16 @@ namespace Galleass.Controllers
             System.Console.WriteLine($"X is {x}, Y is {y}");
             int? slot = HttpContext.Session.GetInt32("PlayerId");
             int? id = HttpContext.Session.GetInt32("UserId");
+
             Player Playing = dbContext.Players.FirstOrDefault(p => p.Slot == (int)slot && p.UserId == (int)id);
+
             GridSquare newPosition = dbContext.GridSquares.FirstOrDefault(g => g.xCoord == x && g.yCoord ==y);
+
             Playing.GridSquareId = newPosition.GridSquareId;
+
             HttpContext.Session.SetInt32("X",x);
             HttpContext.Session.SetInt32("Y",y);
+            dbContext.SaveChanges();
             return "success";
         }
     }
